cff9f52313444c6d367a9476f95dc810d8249b3a
Myroslav Shulha <myrosllavsh@gmail.com>
2025-09-25 15:45:42 +0300

    MrAnt

--- index.js
+++ index.js
@@ -1,9 +1,1 @@
-console.log(10);
-console.log(20);
-console.log(30);
-console.log(40);
-console.log(50);
-console.log(60);
-console.log(70);
-console.log(80);
-console.log(90);
+console.log("MrAnt");
